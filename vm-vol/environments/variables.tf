8aabe688e25588480c048974e364cfff99ba2fad
sd2899 <34939012+sd2899@users.noreply.github.com>
2025-11-05 16:47:05 +0530

    Update variables.tf

--- vm-vol/environments/variables.tf
+++ vm-vol/environments/variables.tf
@@ -2,4 +2,5 @@ variable "ubuntu_image_id" {}
 variable "centos_image_id" {}
 variable "flavor" {}
 variable "keypair" {}
-variable "network_name" {
+variable "network_name" {}
+variable "external_network" {}
