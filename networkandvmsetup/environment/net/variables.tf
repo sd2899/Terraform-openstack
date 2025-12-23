60cb66dabde510565abea17688adfff715e92db2
sd2899 <34939012+sd2899@users.noreply.github.com>
2025-12-23 12:04:47 +0530

    Create variables.tf

---
+++ networkandvmsetup/environment/net/variables.tf
@@ -0,0 +1,37 @@
+variable "external_network_name" {
+  description = "External provider network"
+  type        = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "domain_id" {
+  type = string
+}
+
+
+variable "networks" {
+  description = "Networks with optional router configuration"
+  type = map(object({
+
+    router = object({
+      create            = bool
+      name              = string
+      use_existing      = bool
+      shared_router     = bool
+    })
+
+    subnets = map(object({
+      cidr    = string
+      gateway = string
+      dns     = list(string)
+      dhcp    = bool
+    }))
+  }))
+}
+
+variable "vms" {
+  type = any
+}
